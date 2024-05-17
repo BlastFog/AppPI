2f63e26ad37cc808ad9a4b73e339eab7f776de29
Martin <121202238+BlastFog@users.noreply.github.com>
2024-05-17 15:57:46 +0200

    Added basic app structure

---
+++ app/src/main/java/com/example/apppi/ui/main/Fragment2.kt
@@ -0,0 +1,37 @@
+package com.example.apppi.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.apppi.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment2 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false)
+    }
+
+    companion object {
+        fun newInstance() = Fragment2()
+    }
+}
---
+++ app/src/main/java/com/example/apppi/ui/main/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.example.apppi.ui.main
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.apppi.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var startBut: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        startBut = view.findViewById(R.id.startBut)
+        startBut.setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.fragment_container,Fragment2.newInstance()).addToBackStack(null).commit()
+        }
+
+        return view
+    }
+}
