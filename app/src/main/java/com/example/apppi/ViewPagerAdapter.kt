f91028451db610f820c48c119f7fc0cd945a234e
Martin <121202238+BlastFog@users.noreply.github.com>
2024-06-30 13:19:35 +0200

    Added 4th Fragment

--- app/src/main/java/com/example/apppi/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/apppi/ViewPagerAdapter.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.apppi.ui.main.Fragment2
 import com.example.apppi.ui.main.Fragment3
+import com.example.apppi.ui.main.Fragment4
 
 class ViewPagerAdapter (activity: FragmentActivity) : FragmentStateAdapter(activity) {
     private val fragments = listOf(
         Fragment2(),
-        Fragment3()
+        Fragment3(),
+        Fragment4()
     )
 
     override fun getItemCount(): Int = fragments.size
