6511c0b2a4fc810982c0ce8494a811ae716788b1
Martin <121202238+BlastFog@users.noreply.github.com>
2024-06-30 13:56:11 +0200

    Added QueryStringBuilder using a Map

--- app/src/main/java/com/example/apppi/ui/main/Fragment3.kt
+++ app/src/main/java/com/example/apppi/ui/main/Fragment3.kt
@@ -1,17 +1,26 @@
 package com.example.apppi.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.apppi.QueryStringBuilder
 import com.example.apppi.R
+import org.chromium.net.CronetEngine
+import org.chromium.net.UrlRequest
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 
 class Fragment3 : Fragment() {
 // Youtube API Interaction Fragment (Auth Token)
 // Order: 1.  GET https://www.googleapis.com/youtube/v3/search params: part=snippet, q=@..., type=channel, key=...
 // 2. GET https://www.googleapis.com/youtube/v3/channels params: part=statistics, id=oldQuery[items[statistics]].subcriberCount, key=...
+
+    private lateinit var ytFetchBut: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,7 +31,37 @@ class Fragment3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false)
+        val view = inflater.inflate(R.layout.fragment_3, container, false)
+
+        ytFetchBut = view.findViewById(R.id.ytFetch)
+        ytFetchBut.setOnClickListener{
+            val myBuilder = CronetEngine.Builder(context)
+            val cronetEngine: CronetEngine = myBuilder.build()
+            val executor: Executor = Executors.newSingleThreadExecutor()
+            //val apiUrl = "https://www.googleapis.com/youtube/v3/search"
+
+            val channelName : String = "SB737"
+
+            val queries : Map<String, String> = mapOf("part" to "snippet", "q" to "@$channelName", "type" to "channel", "key" to "123")
+
+            val apiUrl = QueryStringBuilder.newInstance().buildQueryString("https://www.googleapis.com/youtube/v3/search", queries)
+
+            Log.i("testCalls101",apiUrl)
+
+            val requestBuilder = cronetEngine.newUrlRequestBuilder(
+                apiUrl,
+                MyUrlRequestCallback(),
+                executor
+            )
+
+            requestBuilder.addHeader("Content-Type","application/json")
+
+
+            val request: UrlRequest = requestBuilder.build()
+            //request.start()
+        }
+
+        return view
     }
 
     companion object {
